f247b75dcde2d56fffe80d2ebbcb91523ddb1174
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-03-08 10:57:46 -0600

    modified schema to create a job_postings table

---
+++ db/schema.rb
@@ -0,0 +1,26 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_03_08_161435) do
+
+  create_table "job_postings", force: :cascade do |t|
+    t.string "position_name"
+    t.string "position_desc"
+    t.string "location"
+    t.string "benefits"
+    t.integer "employer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employer_id"], name: "index_job_postings_on_employer_id"
+  end
+
+end
