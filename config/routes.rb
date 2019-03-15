fddab3024ee1ff82a5d6d13a89f2f0d4d543dae3
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-03-15 18:12:27 -0500

    added route for root url; made job_postings have nested resources
    Root route:
    - GET request goes to the index on the job_postings controller (2)
    
    Nested Resources: (3-5)
    - because of the association of job_postings and employers
    (job_posting has one employer, employer has many job_postings)
    I felt nested resources would make sense for navigation
    - :job_postings are nested under :employers
    - included every route provided by resources helper except for :index
      - default index routes while :job_posting has nested resources
      didn't make sense for how the page was being used

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :employers
-  resources :job_postings
+  root 'job_postings#index'
+  resources :employers do
+    resources :job_postings, only: [:new, :create, :show, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
