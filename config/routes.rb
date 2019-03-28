a79487fc4ecb481b6310a078b95472f13a74f878
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-03-28 14:48:08 -0500

    switched resources to shallow nesting, added comments resources
    - Reason for shallow nesting (5 - 8):
      - because current relationship between tables is
      employers <- job_postings -> comments nesting is necessary
      - keeping nesting shallow allows for less complicated url paths
      - also removes unnecessary parameters getting passed where that
      info is not being used -> reducing potential unnecessary SQL queries
    
    - removal of nested job_posting index route (6)
      - structure of url route doesn't make sense for info displayed
    
    - comments resources (7)
      - only need :new and :create routes for the context of my application
      - still make sense to be deeply nested as I need
    
    - non-nested index resource for job_postings (10)
      - "job_postings/"
      - just an alias for the index page, may change

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root 'job_postings#index'
   get 'job_postings/new', to: 'job_postings#new_with_unknown_employer'
   post 'job_postings/create', to: 'job_postings#create_with_unknown_employer'
-  resources :employers do
-    resources :job_postings, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :employers, shallow: true do
+    resources :job_postings, except: [:index]  do
+      resources :comments, only: [:new, :create]
+    end
   end
+  resources :job_postings, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
