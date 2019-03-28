1153896ab3b22b4ed28a3459270c68c7c39d09ef
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-03-28 13:34:37 -0500

    added a create method and private comment_params method
    - create method (2 - 6)
       - finds the job_posting to create the comment under
       via the job_posting_id passed into url params (3)
       - creates and saves a new comment under the job_posting using
       comment_params helper (4)
      - redirects to the details page of the job_posting that the comment
      was posted on (5)
    
    - comment_params (9 - 11)
      - helper that allows only the title and body of a comment to be set

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+    def create
+        @job_posting = JobPosting.find(params[:job_posting_id])
+        @comment = @job_posting.comments.create(comment_params)
+        redirect_to job_posting_path(@job_posting)
+    end
+
+    private
+    def comment_params 
+        params.require(:comment).permit(:title, :body)
+    end
 end
