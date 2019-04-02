54303e3c7cb1e742618454401308dc84b6f222b1
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-02 14:56:12 -0500

    changed root route to landing page; split routes b/w admin/user ports
    - root route change (3)
      - goes to landing action of staticPages controller
    
    - admin routes (6 - 22)
      - adds routes for admin port
      - used scope instead of namespace so I could continue to use
      general controllers of my three models, while having new admin-
      specific helpers and paths (6)
      - resource structure and content is identical to previous commit
      (9 - 22)
    
    - user routes (26 - 42)
      - adds routes for user port
      - used scope instead of namespace so I could continue to use
      general controllers of my three models, while having new user-
      specific helpers and paths (26)
      - resource structure and content is identical to previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,44 @@
 Rails.application.routes.draw do
-  root 'job_postings#index'
-  get 'job_postings/new', to: 'job_postings#new_with_unknown_employer'
-  post 'job_postings/create', to: 'job_postings#create_with_unknown_employer'
-  resources :employers, shallow: true do
-    resources :job_postings, except: [:index]  do
-      resources :comments, only: [:new, :create]
+  # root url should go custom landing page
+  root 'static_pages#landing'
+
+  # routes associated with admin port
+  scope :admin, as: 'admin' do
+
+    # builds admin versions of these routes 
+    get 'job_postings/new', to: 'job_postings#new_with_unknown_employer'
+    post 'job_postings/create', to: 'job_postings#create_with_unknown_employer'
+
+    resources :employers, shallow: true do
+      resources :job_postings, except: [:index]  do
+
+        # only need to create a comment for now
+        resources :comments, only: [:new, :create]
+      end
     end
+
+    # index is be routed to 'admin/job_postings'
+    resources :job_postings, only: [:index]
   end
-  resources :job_postings, only: [:index]
+
+
+  # routes associated with user port
+  scope :user, as: 'user' do
+
+    # builds user versions of these routes 
+    get 'job_postings/new', to: 'job_postings#new_with_unknown_employer'
+    post 'job_postings/create', to: 'job_postings#create_with_unknown_employer'
+
+    resources :employers, shallow: true do
+      resources :job_postings, except: [:index]  do
+
+        # only need to create a comment for now
+        resources :comments, only: [:new, :create]
+      end
+    end
+
+    # index is routed to 'user/job_postings'
+    resources :job_postings, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
