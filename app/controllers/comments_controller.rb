d0ddbf58d80dbc5a1aa76c4b8f531c2014276af8
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-03 15:21:15 -0500

    added is_admin and called it before create action for @admin-based logic
    - is_admin (18 - 21)
      - helper method for setting the @admin instance var
      - @admin is true if the first part of the request url is "admin"
      - false otherwise
    
    - create has logic for redirecting to the proper page
      - if @admin is true, admin path is used
      - else, user path is used

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,22 @@
 class CommentsController < ApplicationController
+    before_action :is_admin, only: [:create]
     def create
         @job_posting = JobPosting.find(params[:job_posting_id])
         @comment = @job_posting.comments.create(comment_params)
-        redirect_to job_posting_path(@job_posting)
+        if @admin
+            redirect_to admin_job_posting_path(@job_posting)
+        else
+            redirect_to user_job_posting_path(@job_posting)
+        end
     end
 
     private
     def comment_params 
         params.require(:comment).permit(:title, :body)
+    end
+
+    def is_admin
+        port = request.fullpath.split("/")[1]
+        @admin = (port == "admin")
     end
 end
