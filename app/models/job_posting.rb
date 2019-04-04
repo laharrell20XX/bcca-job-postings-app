3fe30da8ab96b7616b0b7eb1632ed34904a5280f
laharrell20XX <lharrell@basecampcodingacademy.org>
2019-04-04 14:29:59 -0500

    Added helper methods for sorting job_postings
    - self.alphabetically (5 - 7)
      - class method that orders job_postings alphabetically by
      position_name
      - calling order on JobPosting ActiveRecord object generates
      order by sql statement
    
    - self.reverse_alphabetically (9 -11)
      - class method that orders job_postings reverse alphabetically by
      position_name
    
    - self.oldest_newest_created (13 - 15)
      - class method that orders job_postings with oldest created first
      to newest created last
    
    - self.newest_oldest_created (17 - 19)
      - class method that orders job_postings with newest created first
      to oldest created last

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,4 +1,25 @@
 class JobPosting < ApplicationRecord
   belongs_to :employer
   has_many :comments
+
+  # orders job_postings alphabetically by position_name
+  def self.alphabetically
+    JobPosting.order(:position_name)
+  end
+
+  # orders job_postings reverse alphabetically by position_name
+  def self.reverse_alphabetically
+    JobPosting.order(position_name: :desc)
+  end
+
+  # orders job_postings with oldest created first, newest created last
+  def self.oldest_newest_created
+    JobPosting.order(:created_at)
+  end
+
+  # orders job_postings with newest created first, oldest created last
+  def self.newest_oldest_created
+    JobPosting.order(created_at: :desc)
+  end
+
 end
